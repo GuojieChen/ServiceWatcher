67b89f3708d3ca32746893c23e81f9025021c94d
efflicto <sven.soost@gmail.com>
2015-02-09 09:23:53 +0100

    ExceptionHandling

--- Program.cs
+++ Program.cs
@@ -16,21 +16,29 @@ namespace ServiceWatcher {
             while (true) {
 
                 foreach (string service in services) {
-                    Console.WriteLine(DateTime.Now + " Checking status of: {0}", service);
-                    ServiceController sc = new ServiceController(service);
-                    Console.WriteLine(DateTime.Now + " Status of service {0} is: {1}", service, sc.Status);
-                    if (sc.Status != ServiceControllerStatus.Running) {
-                        Console.WriteLine(DateTime.Now + " Start service {0}", sc.DisplayName);
-                        sc.Start();
-                        sc.WaitForStatus(ServiceControllerStatus.Running);
-                        if (sc.Status != ServiceControllerStatus.Running && sc.Status != ServiceControllerStatus.StartPending) {
-                            Console.WriteLine(DateTime.Now + " Service {0} not started properly - retry", sc.DisplayName);
-                            sc.Start();
-                        }
+                    try {
 
+                        Console.WriteLine(DateTime.Now + " Checking status of: {0}", service);
+                        ServiceController sc = new ServiceController(service);
+                        Console.WriteLine(DateTime.Now + " Status of service {0} is: {1}", service, sc.Status);
+                        if (sc.Status != ServiceControllerStatus.Running) {
+                            Console.WriteLine(DateTime.Now + " Start service {0}", sc.DisplayName);
+                            sc.Start();
+                            sc.WaitForStatus(ServiceControllerStatus.Running);
+                            if (sc.Status != ServiceControllerStatus.Running && sc.Status != ServiceControllerStatus.StartPending) {
+                                Console.WriteLine(DateTime.Now + " Service {0} not started properly - retry", sc.DisplayName);
+                                sc.Start();
+                            }
+
+                        }
+                        sc.Dispose();
+                        sc = null;
+
+                    } catch (Exception e) {
+
+                        Console.WriteLine("Error: {0}", e.StackTrace);
                     }
-                    sc.Dispose();
-                    sc = null;
+
                 }
                 Console.WriteLine(DateTime.Now + " All done. Waiting for {0} sec", Properties.Settings.Default.Delay / 1000);
                 System.Threading.Thread.Sleep(ServiceWatcher.Properties.Settings.Default.Delay);
